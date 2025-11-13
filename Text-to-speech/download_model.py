b9310131dfe9b0a344cae576a2d15ef8f38b73b1
A-ges <andrey.geschiere@icloud.com>
2025-11-13 22:10:00 +0100

    Refactor print statements in download_model.py

--- Text-to-speech/download_model.py
+++ Text-to-speech/download_model.py
@@ -1,11 +1,12 @@
 from TTS.api import TTS
 import torch
 
-print("--- Downloading and caching the pre-trained model ---")
+print("Downloading and caching the pre-trained model")
+print()
 
-# This line will connect to the internet, download the required model files,
-# and save them in the correct location for future use.
+#This line will connect to the internet, download the required model files and save them in the correct location for future use.
 tts = TTS("tts_models/en/ljspeech/tacotron2-DDC_ph", progress_bar=True, gpu=torch.cuda.is_available())
 
-print("\n--- Model download complete. ---")
-print("You can now run the train_tts.py script.")
+print("Model download complete")
+print("You can now run the train_tts.py script")
+
