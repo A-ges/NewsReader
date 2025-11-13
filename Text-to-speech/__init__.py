fd7d0acb5ca0fc68d91171059458dda5824b0fbe
A-ges <andrey.geschiere@icloud.com>
2025-11-13 22:08:55 +0100

    Restore Bangla phonemizer import and setup

--- Text-to-speech/__init__.py
+++ Text-to-speech/__init__.py
@@ -1,4 +1,4 @@
-# from TTS.tts.utils.text.phonemizers.bangla_phonemizer import BN_Phonemizer
+from TTS.tts.utils.text.phonemizers.bangla_phonemizer import BN_Phonemizer
 from TTS.tts.utils.text.phonemizers.base import BasePhonemizer
 from TTS.tts.utils.text.phonemizers.belarusian_phonemizer import BEL_Phonemizer
 from TTS.tts.utils.text.phonemizers.espeak_wrapper import ESpeak
@@ -19,19 +19,19 @@ ESPEAK_LANGS = list(ESpeak.supported_languages().keys())
 GRUUT_LANGS = list(Gruut.supported_languages())
 
 
-# Dict setting default phonemizers for each language
-# Add Gruut languages
+#Dict setting default phonemizers for each language
+#Add Gruut languages
 _ = [Gruut.name()] * len(GRUUT_LANGS)
 DEF_LANG_TO_PHONEMIZER = dict(list(zip(GRUUT_LANGS, _)))
 
 
-# Add ESpeak languages and override any existing ones
+#Add ESpeak languages and override any existing ones
 _ = [ESpeak.name()] * len(ESPEAK_LANGS)
 _new_dict = dict(list(zip(list(ESPEAK_LANGS), _)))
 DEF_LANG_TO_PHONEMIZER.update(_new_dict)
 
 
-# Force default for some languages
+#Force default for some languages
 DEF_LANG_TO_PHONEMIZER["en"] = DEF_LANG_TO_PHONEMIZER["en-us"]
 DEF_LANG_TO_PHONEMIZER["zh-cn"] = ZH_CN_Phonemizer.name()
 DEF_LANG_TO_PHONEMIZER["ko-kr"] = KO_KR_Phonemizer.name()
@@ -39,23 +39,15 @@ DEF_LANG_TO_PHONEMIZER["ko-kr"] = KO_KR_Phonemizer.name()
 DEF_LANG_TO_PHONEMIZER["be"] = BEL_Phonemizer.name()
 
 
-# JA phonemizer has deal breaking dependencies like MeCab for some systems.
-# So we only have it when we have it.
+#JA phonemizer has deal breaking dependencies like MeCab for some systems
+#So we only have it when we have it
 if JA_JP_Phonemizer is not None:
     PHONEMIZERS[JA_JP_Phonemizer.name()] = JA_JP_Phonemizer
     DEF_LANG_TO_PHONEMIZER["ja-jp"] = JA_JP_Phonemizer.name()
 
 
 def get_phonemizer_by_name(name: str, **kwargs) -> BasePhonemizer:
-    """Initiate a phonemizer by name
-
-    Args:
-        name (str):
-            Name of the phonemizer that should match `phonemizer.name()`.
-
-        kwargs (dict):
-            Extra keyword arguments that should be passed to the phonemizer.
-    """
+    #Initiate a phonemizer by name
     if name == "espeak":
         return ESpeak(**kwargs)
     if name == "gruut":
@@ -79,3 +71,4 @@ def get_phonemizer_by_name(name: str, **kwargs) -> BasePhonemizer:
 
 if __name__ == "__main__":
     print(DEF_LANG_TO_PHONEMIZER)
+
